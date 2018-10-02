ce977866113a6b63ac55c215cedfd72d6104749e
Kamil Żabiński <kamil.adam.zabinski@gmail.com>
2018-10-02 21:54:51 +0200

    fix & run

--- re/js/src/main/scala/pl/writeonly/re/main/Main.scala
+++ re/js/src/main/scala/pl/writeonly/re/main/Main.scala
@@ -1,7 +1,11 @@
+package pl.writeonly.re
+
+import pl.writeonly.re.shared.Core
+
 import scala.scalajs.js
 
 object Main extends js.JSApp {
   def main(): Unit = {
-    println(Bar.a)
+    Core.apply(Array())
   }
 }
